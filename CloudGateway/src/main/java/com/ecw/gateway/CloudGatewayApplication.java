f8479132ef96dad666e33d7ab3a48ab0d76bb797
dinesh.kakani <dinesh.kakani@eclinicalworks.com>
2024-07-05 10:46:31 +0530

    Reason: Working fine with gateway

--- CloudGateway/src/main/java/com/ecw/gateway/CloudGatewayApplication.java
+++ CloudGateway/src/main/java/com/ecw/gateway/CloudGatewayApplication.java
@@ -1,10 +1,20 @@
 package com.ecw.gateway;
 
+import io.github.resilience4j.circuitbreaker.CircuitBreakerConfig;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.circuitbreaker.resilience4j.Resilience4JCircuitBreaker;
+import org.springframework.cloud.circuitbreaker.resilience4j.Resilience4JCircuitBreakerFactory;
+import org.springframework.cloud.circuitbreaker.resilience4j.Resilience4JConfigBuilder;
+import org.springframework.cloud.client.circuitbreaker.CircuitBreaker;
+import org.springframework.cloud.client.circuitbreaker.Customizer;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.gateway.filter.GatewayFilter;
+import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.cloud.gateway.filter.factory.GatewayFilterFactory;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 @EnableDiscoveryClient
@@ -14,6 +24,14 @@ public class CloudGatewayApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CloudGatewayApplication.class, args);
+	}
+	@Bean
+	public Customizer<Resilience4JCircuitBreakerFactory> getDefaultCircuitBreaker() {
+		return factory -> factory.configureDefault(
+				id -> new Resilience4JConfigBuilder(id).circuitBreakerConfig(
+						CircuitBreakerConfig.ofDefaults()
+				).build()
+		);
 	}
 
 	/*@Bean
